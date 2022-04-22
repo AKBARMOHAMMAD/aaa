0ca95acceb3eb1c0fce322ffec808b326280f5c1
AKBAR MOHAMMAD <akbarmd533@gmail.com>
2022-04-22 15:45:46 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -40,11 +40,13 @@ variable "public_subnet_1_cidr" {
   }
 
   variable "Access_key_ID" {
+    default ="AKIAX2MAADMD22K6WNAI"
     description = "Access key of aws"
     type = string
   }
 
   variable "Secret_access_key" {
+    default ="zDCPJlbT4/NPiHpDs/w8MYu/5bROFhQxPabxml15"
     description = "Secret Access key of AWS"
     type = string
   }
